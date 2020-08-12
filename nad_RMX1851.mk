6820ceec7a87a412ea9635113887a28be6de6134
andrraa <andraserver01@gmail.com>
2020-08-12 00:38:49 +0000

    RMX1851: Remove GAPPS flag

--- nad_RMX1851.mk
+++ nad_RMX1851.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Specify arch for gapps
-TARGET_GAPPS_ARCH=arm64
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
